76bac1156b6b2d1c68e143327715618f1aa8567e
Yingwen Tan <tanyingwen@ust.hk>
2026-09-03 17:03:08 +0800

    Include search files in GitHub Pages build

--- scripts/prepare-github-pages.mjs
+++ scripts/prepare-github-pages.mjs
@@ -1,4 +1,4 @@
-import { copyFile, mkdir } from 'node:fs/promises';
+import { copyFile, mkdir, writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
 
 const outputDir = join(process.cwd(), 'dist', 'client');
@@ -8,3 +8,19 @@ for (const route of ['research', 'cv']) {
   await mkdir(routeDir, { recursive: true });
   await copyFile(join(outputDir, `${route}.html`), join(routeDir, 'index.html'));
 }
+
+await writeFile(
+  join(outputDir, 'robots.txt'),
+  'User-Agent: *\nAllow: /\n\nSitemap: https://forestknowswhy.github.io/sitemap.xml\n',
+);
+
+await writeFile(
+  join(outputDir, 'sitemap.xml'),
+  `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url><loc>https://forestknowswhy.github.io/</loc></url>
+  <url><loc>https://forestknowswhy.github.io/research/</loc></url>
+  <url><loc>https://forestknowswhy.github.io/cv/</loc></url>
+</urlset>
+`,
+);
